d66f6c01708377b18b2fb0370c22e60497ae7b4e
oraimagini <mompyn@gmail.com>
2022-05-25 22:23:37 -0400

    server

--- node-temp-cerveceria/server/index.js
+++ node-temp-cerveceria/server/index.js
@@ -1,3 +1,15 @@
+// server
+const http = require('http');
+const express = require('express');
+//const io = require('socketio');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+server.listen(3000, function () {
+   console.log('server listening on port', 3000)
+})
 
 // serialport biblioteca para conectar con arduino
 const { SerialPort } = require('serialport');
@@ -11,4 +23,6 @@ var dataArduino;
 // formato de información por consola 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-parser.on('data', console.log)
+parser.on('data', function (data){
+   console.log(data)
+})
